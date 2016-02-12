118d61388487a56a88b94a52106c5b58d8e5d0a6
lhoag <lhoag@yahoo.com>
2016-02-11 21:53:24 -0500

    Up to cleaning input.

--- ProcessTimesheets/classes.py
+++ ProcessTimesheets/classes.py
@@ -1,6 +1,7 @@
 import os
 import datetime
 import logging
+from openpyxl import load_workbook
 
 #-----------------------------------------------------------------------
 class FAEMember:
@@ -190,10 +191,12 @@ class Week:
 #    logging.debug(str(self.wsDate) + ' ' + self.fname + ' ' + self.lname)
 
 
+#-----------------------------------------------------------------------
 class RegionReport:
   def __init__(self):
     self.week = {}
 
+#-----------------------------------------------------------------------
 class TSCalendar:
   def __init__(self,year):
     self.week = {}
@@ -220,24 +223,206 @@ class TSCalendar:
       logging.debug(str(i) + ' ' + str(self.week[i])) 
  
 
+#-----------------------------------------------------------------------
+# Class Timesheet Entry
+#-----------------------------------------------------------------------
+class TSEntry:
+  def __init__(self,date,partA,partB,partC,partD,hours,ltd,notes):
+    self.date  = date
+    self.partA = partA
+    self.partB = partB
+    self.partC = partC
+    self.partD = partD
+    self.hours = hours
+    self.ltd   = ltd
+    self.notes = notes
 
+  def Log(self):
+    date  = self.date
+    partA = self.partA.ljust(50)
+    partB = self.partB.ljust(60)
+    partC = self.partC.ljust(45)
+    partD = self.partD.ljust(35)
+    hours = str(self.hours).ljust(5)
+    ltd   = self.ltd.ljust(10)
+    logging.debug(str(date) + '|' + partA + '|' + partB + '|' + partC + '|' + partD + '|' + hours + '|' + ltd)
 
+#-----------------------------------------------------------------------
+# Class Timesheet
+#-----------------------------------------------------------------------
+class Timesheet:
+  def __init__(self, ssdata, wsDate):
+    self.entries = []
+    self.ssdata  = ssdata
+    self.wsDate  = wsDate
+
+  #---------------------------------------------------------------------
+  def calcDate(self,day):
+    day = day.lower().strip()
+    day = day[0:3]
+    day = { 'mon':0,'tue':1,'wed':2,'thu':3,'fri':4,'sat':5,'sun':6 }.get(day,7)
+    if (day == 7):
+      logging.error('Invalid Day')
+    date = self.wsDate + datetime.timedelta(days = day)
+    return date
+
+  #---------------------------------------------------------------------
+  def Clean(self):
+    for i,entry in enumerate(self.entries):
+      entry.Log()
+
+  #---------------------------------------------------------------------
+  def ReadFile(self):
+    path = self.ssdata.filename
+    logging.debug(path)
+    wb = load_workbook(path)
+    sheet_names = wb.get_sheet_names();
+    if (sheet_names[0] != 'Timesheet'):
+      logging.error(path + ' is not a valid Timesheet spreadsheet')
+
+    ws = wb.get_sheet_by_name('Timesheet')
+
+    wsRow     = 7
+    wsCol     = 4
+    sundayFlg = False
+    sundayCnt = 0;
+    blankFlg  = False
+    curDate   = None
+
+    #syncCell = ws.cell(row=6,column=4)
+    #logging.debug(syncCell)
+    if (ws.cell(row=6,column=4).value == 'Customer - Part A'): 
+      wsCol = 3
+    elif (ws.cell(row=6,column=5).value == 'Customer - Part A'):
+      wsCol = 4
+    else:
+      logging.error('Couldn\'t sync to Timesheet spreadsheet')
+      return
+
+    while True:
+      day = ws.cell(row=wsRow,column=wsCol+0).value
+      if (day == None):
+        day = ''
+        if (curDate == None):
+          logging.error('SHOULDNT BE HERE')
+      else:
+        curDate = self.calcDate(day)
+ 
+      partA  = ws.cell(row=wsRow,column=wsCol+1).value
+      if (partA == None):
+        partA = ''
+        blankFlg = True
+
+      partB = ws.cell(row=wsRow,column=wsCol+2).value
+      if (partB == None):
+        partB = ''
+
+      partC = ws.cell(row=wsRow,column=wsCol+3).value
+      if (partC == None):
+        partC = ''
+
+      partD = ws.cell(row=wsRow,column=wsCol+4).value
+      if (partD == None):
+        partD = ''
+
+      hours = ws.cell(row=wsRow,column=wsCol+9).value
+      #logging.debug('hours ' + str(hours))
+      if (hours == None):
+        hours = ''
+      else:
+        try:
+          hours = float(hours)
+        except:
+          hours = ''
+      #logging.debug('hours ' + str(hours))
+      #logging.debug('hours ' + str(hours))
+
+      ltd = ws.cell(row=wsRow,column=wsCol+10).value
+      if (ltd == None):
+        ltd = ''
+
+      notes = ws.cell(row=wsRow,column=wsCol+11).value
+      if (notes == None):
+        notes = ''
+ 
+      # TODO: if blankFlg == False, then make sure everything else is blank
+
+      if (not blankFlg):
+        #if (day == ''):
+        #  logging.error('Day is blank')
+        #logging.debug(curDay + '|' + partA + '|' + partB + '|' + partC + '|' + '|')
+        self.entries.append(TSEntry(curDate,partA,partB,partC,partD,hours,ltd,notes))
+
+      if (day.startswith('Sunday')):
+        sundayFlg = True
+      if (sundayFlg):
+        if (blankFlg):
+          sundayCnt += 1
+        else:
+          sundayCnt = 0;
+      if (sundayCnt > 5):
+        break
+
+      wsRow += 1
+      blankFlg = False
+
+    #logging.debug('Done ReadFile')
+
+  def Log(self):
+    for i,entry in enumerate(self.entries):
+      fullname = self.ssdata.fae.fullname.ljust(25)
+      date  = self.entries[i].date
+      partA = self.entries[i].partA.ljust(50)
+      partB = self.entries[i].partB.ljust(60)
+      partC = self.entries[i].partC.ljust(45)
+      partD = self.entries[i].partD.ljust(35)
+      hours = str(self.entries[i].hours).ljust(5)
+      ltd   = self.entries[i].ltd.ljust(10)
+      logging.debug(fullname + '|' + str(date) + '|' + partA + '|' + partB + '|' + partC + '|' + partD + '|' + hours + '|' + ltd)
+
+#-----------------------------------------------------------------------
+# Class TSSummary
+#-----------------------------------------------------------------------
 class TSSummary:
   def __init__(self):
     self.sslist = []
+    self.tsdict = {}
+    self.week = None
+    self.year = None
 
-
+  #---------------------------------------------------------------------
   def Process(self,tsdata,team,year,week):
+    self.year = year
+    self.week = week
+
     cal = TSCalendar(year)
     self.Sort(tsdata,cal,week)
 
+    for i,week in enumerate(self.sslist):
+      wsDate = cal.week[i+1]
+      logging.debug('***' + str(wsDate) + '***')
+      self.tsdict[wsDate] = []
+      for j,ssdata in enumerate(week):
+        ts = Timesheet(ssdata, wsDate)
+        ts.ReadFile()
+        ts.Clean()
+        self.tsdict[wsDate].append(ts)
+      logging.debug('***' + str(wsDate) + '***')
+
+    #for i in range(0,self.week-1):
+    #  wsDate = cal.week[i+1]
+    #  for j,ts in enumerate(self.tsdict[wsDate]):
+    #    self.tsdict[wsDate][j].Log()
+
+  #-----------------------------------------------------------------------
   def Sort(self,tsdata,cal,week):
     self.sslist = []
     for i in range(1,week+1):
       list = []
       dict = {}
-      logging.debug(str(i) + ' ' + str(cal.week[i]))
-      dict = tsdata.weeks[cal.week[i]]
+      wsDate = cal.week[i]
+      logging.debug(str(i) + ' ' + str(wsDate))
+      dict = tsdata.weeks[wsDate]
       for key,value in dict.items():
         list.append(value)
       self.sslist.append(sorted(list))
